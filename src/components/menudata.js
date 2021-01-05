4653e329e3ec849c6a1734783461ad739d62eefb
swordslinger <seanskelton40@gmail.com>
2021-01-05 23:56:01 +0000

    finished some api work

--- src/components/menudata.js
+++ src/components/menudata.js
@@ -1,50 +1,32 @@
 //imports
+import axios from 'axios'
 import React from 'react'
 import { Menu } from './menu'
-
 //export component for use in project
 export class MenuData extends React.Component {
 
-    menuData = {
-        menu: [
-            {
-                "FoodName": "Garlic Bread",
-                "Price": "5.00",
-                "imdbID": "0",
-                "Type": "food",
-                "FoodPicture": "https://thumbs.dreamstime.com/b/garlic-pizza-bread-5951445.jpg"
-            },
-            {
-                "FoodName": "Pizza",
-                "Price": "14.00",
-                "imdbID": "1",
-                "Type": "food",
-                "FoodPicture": "https://thumbs.dreamstime.com/b/italian-pepperoni-pizza-thick-pie-crust-spicy-sausage-mozzarella-tomato-isolated-white-viewed-whole-80010925.jpg"
-            },
-            {
-                "FoodName": "Pasta",
-                "Price": "10.00",
-                "foodID": "2",
-                "Type": "food",
-                "FoodPicture": "https://thumbs.dreamstime.com/b/pasta-close-up-penne-parmesan-cheese-fresh-onion-97533239.jpg"
-            },
-            {
-                "FoodName": "Chips",
-                "Price": "2.99",
-                "foodID": "3",
-                "Type": "food",
-                "FoodPicture": "https://thumbs.dreamstime.com/b/fat-chips-8972481.jpg"
-            }
-        ]
+    state = {
+        menu: [ ]
     }
         
+    componentDidMount(){
+        axios.get('https://jsonblob.com/api/c572eeef-4f94-11eb-bace-e9e856984632')
+        .then(
+            (response)=>{
+                this.setState({menu: response.data.Search})
+                console.log(response.data.Search)
 
+            })
+        .catch((error)=>{
+            console.log(error)
+        });
+    }
 
 render(){
     return (
         <div>
             <h1>This is the menu Data Component</h1>
-            <Menu menu = {this.menuData.menu}></Menu>
+            <Menu menu = {this.state.menu}></Menu>
         </div>
         )
     }
